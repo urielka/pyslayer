86c38fc4bf06d4658ee7d9cc5b63b12775e4022c
Uriel Katz <uriel.katz@gmail.com>
2011-06-05 16:29:41 +0300

    setup.py and stuff

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='pyslayer',
+      version='0.1',
+      packages=['pyslayer'],
+      install_requires = ['MySQLdb'],
+      extras_require = {'process title': ["setproctitle"],'daemonize':["python-daemon"]},
+      author = "Uriel Katz",
+      author_email = "uriel.katz@gmail.com",
+      description = "a python clone of dbslayer - JSON HTTP abstraction for DBAPI",
+      license = "MIT",
+      keywords = "dbslayer json dbapi",
+      url = "https://github.com/urielka/pyslayer",
+      download_url = "https://github.com/urielka/pyslayer/downloads",
+      zip_safe = False,
+      entry_points = '''
+      [console_scripts]
+      pyslayer = pyslayer.main:cli_start
+      ''',
+)
